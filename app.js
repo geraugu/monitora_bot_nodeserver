343d1cef92cef5a4fd14398ae6dfdba44c6f8f08
Geraldo A M Figueiredo <geraldo.morais@gmail.com>
2017-03-19 14:37:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,71 +1,112 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-var express    = require('express'),
-  app          = express(),
-  watson       = require('watson-developer-cloud'),
-  extend       = require('util')._extend,
-  i18n         = require('i18next');
+var express = require('express');
 var Parse = require('parse/node');
 const moment = require('moment');
 var bodyParser = require('body-parser')
-//var Monitora = require('./Monitora');
+var Monitora = require('./Monitora');
 
 
 Parse.initialize('JMzpiMhkL1z5hvuGzLhYPppNJPJpoaTAdIp3oNmh', 'mtyHx7hxS1zvPz5FnWq94w4GHzchvb44HJiZOZj2');
 Parse.serverURL = 'http://52.27.220.189/monitoraserver'
 
-//i18n settings
-require('./config/i18n')(app);
+var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Bootstrap application settings
-require('./config/express')(app);
+// parse application/json
+app.use(bodyParser.json())
 
-// Create the service wrapper
-var personalityInsights = watson.personality_insights({
-  version: 'v2',
-  username: '<username>',
-  password: '<password>'
+
+app.post('/messengerbot2/webhook', function (req, res) {
+        res.setHeader('Content-type', 'application/json');
+        var data = req.body;
+        var sender = data.originalRequest.data.sender.id;
+  var metadata = data.result.metadata;
+        var mensagem = data.result.resolvedQuery;
+        var action = data.result.action;
+        var actionIncomplete = data.result.actionIncomplete;
+        var retorno ={};
+        //action = 'input.faltas'
+        switch (action){
+
+                //Se não sabe o que foi digitado, pode ser um nome de político
+                case 'input.unknown':
+                        Monitora.getPolitico(mensagem, function(ret){
+                                                      if(ret.success){
+                                        retorno.data ={
+                                                "facebook" : {
+                                                        "attachment" : {
+                                                                "type" : "template",
+                                                                "payload" : {
+                                                                        "template_type" : "generic",
+                                                                        "elements" : ret.cards
+                                                                }
+                                                        }
+                                                }
+                                        }
+                                }else{
+                                        //se nao encontrar, enviar o fulfillment
+                                        retorno.speech=data.result.fulfillment.speech;
+                                          }
+                                res.send(retorno);
+                        });
+                break;
+
+                //buscar os gastos de acordo com o que o usuário digitou
+                //pode ser
+                //      quem gastou mais até o momento
+                //      quem gastou mais no mes
+                //      quem gastou mais no ano
+                //      qual o maior gasto
+                case 'input.gastos':
+                        Monitora.getGastaMais({mes:null, ano: null,casa:'c'}, function(ret){
+                                retorno.speech=
+                                        "O deputado que mais gastou até agora:\n"+
+                                            ret;
+                                Monitora.getGastaMais({mes:null, ano: null,casa:'s'}, function(ret){
+                                        retorno.speech=retorno.speech+"\n"+
+                                        "O senador que mais gastou até agora:\n"+
+                                        ret;
+                                        res.send(retorno);
+                                });
+
+                        });
+
+                break;
+
+                //buscar as faltas de um político
+                case 'input.faltas':
+                        Monitora.getFaltouMais({mes:null, ano: null}, function(ret){
+                                           "O deputado que mais faltou:\n"+
+                                        ret;
+                                res.send(retorno);
+                        });
+
+                break;
+
+                //seguir um político
+                case 'input.seguir':
+
+
+                break;
+
+                //buscar os processos de um político
+                case 'input.processos':
+
+                break;
+        }
+
+
+        /*console.log('---RESULT-----')
+        console.log(data.result)
+        console.log('----CONTEXTS----')
+        console.log(data.result.contexts)
+        console.log('----FIM----')
+        console.log(data)*/
+        //console.log(data.result.fulfillment
+  
+})
+
+
+app.listen(3000, function () {
+  console.log('Bot listen to 3000!');
 });
-
-app.get('/', function(req, res) {
-  res.render('index', { ct: req._csrfToken });
-});
-
-app.post('/api/profile', function(req, res, next) {
-  var parameters = extend(req.
-                          
-                          
-                          
-                          
-                          , { acceptLanguage : i18n.lng() });
-
-  personalityInsights.profile(parameters, function(err, profile) {
-    if (err)
-      return next(err);
-    else
-      return res.json(profile);
-  });
-});
-
-// error-handler settings
-require('./config/error-handler')(app);
-
-var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
-app.listen(port);
-console.log('listening at:', port);
